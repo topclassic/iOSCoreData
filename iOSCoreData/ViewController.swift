f5cbb5d902a608ef9fa1a40309d8bac20db1324e
Chotipat Poowongthanarat <top_classic@hotmail.com>
2016-09-23 10:05:57 +0700

    Update Swift 3.0

--- iOSCoreData/ViewController.swift
+++ iOSCoreData/ViewController.swift
@@ -14,7 +14,7 @@ class ViewController: UITableViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.Add, target: self, action: #selector(ViewController.addItem))
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: UIBarButtonSystemItem.add, target: self, action: #selector(ViewController.addItem))
         // Do any additional setup after loading the view, typically from a nib.
     }
 
@@ -25,8 +25,8 @@ class ViewController: UITableViewController {
     }
     
     func addItem(){
-        let alert = UIAlertController(title: "Add Item", message: "Please add item", preferredStyle: .Alert)
-        let confirm = UIAlertAction(title: "OK", style: UIAlertActionStyle.Default, handler:({
+        let alert = UIAlertController(title: "Add Item", message: "Please add item", preferredStyle: .alert)
+        let confirm = UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler:({
             (_)in
             if let field = alert.textFields![0] as? UITextField{
                 if field.text != ""{
@@ -37,8 +37,8 @@ class ViewController: UITableViewController {
             }
         }
     ))
-        let  cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.Cancel, handler: nil)
-        alert.addTextFieldWithConfigurationHandler({
+        let  cancelAction = UIAlertAction(title: "Cancel", style: UIAlertActionStyle.cancel, handler: nil)
+        alert.addTextField(configurationHandler: {
             (textField) in
             textField.placeholder = "Your data"
         })
@@ -46,16 +46,16 @@ class ViewController: UITableViewController {
         alert.addAction(cancelAction)
         alert.addAction(confirm)
 
-        self.presentViewController(alert, animated: true, completion: nil)
+        self.present(alert, animated: true, completion: nil)
         
     
     }
     
-    func saveItem(itemToSave : String){
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    func saveItem(_ itemToSave : String){
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext
-        let entity = NSEntityDescription.entityForName("PersonEntity", inManagedObjectContext: managedContext)
-        let item = NSManagedObject(entity: entity!, insertIntoManagedObjectContext: managedContext)
+        let entity = NSEntityDescription.entity(forEntityName: "PersonEntity", in: managedContext)
+        let item = NSManagedObject(entity: entity!, insertInto: managedContext)
         item.setValue(itemToSave, forKey: "firstname")
         do{
             try managedContext.save()
@@ -65,37 +65,36 @@ class ViewController: UITableViewController {
         }
 
     }
-    override func viewWillAppear(animated: Bool) {
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    override func viewWillAppear(_ animated: Bool) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext
-        let fetch = NSFetchRequest(entityName: "PersonEntity")
-
+      //  let fetch = NSFetchRequest(entityName: "PersonEntity")
+        let fetch:NSFetchRequest<NSFetchRequestResult> = NSFetchRequest(entityName: "PersonEntity")
         do{
-            let result = try managedContext.executeFetchRequest(fetch)
+            let result = try managedContext.fetch(fetch)
             person = result as! [NSManagedObject]
         }catch{
             print("error")
         }
     }
-    override func tableView(tableView: UITableView, didSelectRowAtIndexPath indexPath: NSIndexPath) {
-        let appDelegate = UIApplication.sharedApplication().delegate as! AppDelegate
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        let appDelegate = UIApplication.shared.delegate as! AppDelegate
         let managedContext = appDelegate.managedObjectContext
-        tableView.reloadRowsAtIndexPaths([indexPath], withRowAnimation: UITableViewRowAnimation.Right)
-        managedContext.deleteObject(person[indexPath.row])
-        person.removeAtIndex(indexPath.row)
+        tableView.reloadRows(at: [indexPath], with: UITableViewRowAnimation.right)
+        managedContext.delete(person[(indexPath as NSIndexPath).row])
+        person.remove(at: (indexPath as NSIndexPath).row)
         self.tableView.reloadData()
         
     }
 
-    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return person.count
     }
     
-    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCellWithIdentifier("Cell")! as UITableViewCell
-        let item = person[indexPath.row]
-        cell.textLabel?.text = item.valueForKey("firstname") as? String
-        
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Cell")! as UITableViewCell
+        let item = person[(indexPath as NSIndexPath).row]
+        cell.textLabel?.text = item.value(forKey: "firstname") as? String
         return cell
     }
 }
